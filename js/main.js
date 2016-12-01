e484941c347f92e7ed64cc0bafdead352711afb6
aliciabulhoes <aliciabulhoes@gmail.com>
2016-12-01 14:18:29 -0500

    Folder Setup
    Added css, index and js files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//JavaScript Document 
+
+(function(){
+"use strict";
+	
+	console.log("SEAF Fired");//important as a debugging tool//this is a shell
+
+//Variables
+
+//Functions
+
+//Listeners
+
+
+
+})();
